8908da0e7fc37af3403d7bb8c9ea298701ddbfb3
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-12-15 13:36:25 +0100

    Add chatname to schema

--- models/reflection-chat.go
+++ models/reflection-chat.go
@@ -10,6 +10,7 @@ import (
 type ReflectionChat struct {
 	IdReflectionChat uint         `gorm:"primaryKey;column:id_reflection_chat;autoIncrement" json:"-"`
 	ExternalID       uuid.UUID    `gorm:"unique;type:uuid;default:gen_random_uuid()" json:"id"`
+	ChatName         string       `gorm:"column:chat_name"`
 	UserID           uint         `gorm:"column:user_id"`
 	User             User         `gorm:"foreignKey:UserID"`
 	Reflections      []Reflection `gorm:"foreignKey:ChatID"`
